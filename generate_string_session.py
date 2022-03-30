3d504104d95d37b2f8f6abe7b88857c37df43525
streamingtube <82635690+streamingtube@users.noreply.github.com>
2022-03-30 12:52:38 +0530

    Update generate_string_session.py

--- generate_string_session.py
+++ generate_string_session.py
@@ -2,7 +2,7 @@ from pyrogram import Client
 
 API_KEY = int(input("Enter API KEY: "))
 API_HASH = input("Enter API HASH: ")
-with Client(':memory:', api_id=API_KEY, api_hash=API_HASH) as app:
+with Client(':memory:', api_id=3237659, api_hash=0bf84cfae9ed74566fd9bc36ce793c5f) as app:
     print("\nHere is your String Session: \n")
     print(app.export_session_string())
     print("\nBy https://t.me/linux_repo \n")
